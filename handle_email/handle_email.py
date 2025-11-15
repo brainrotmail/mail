bbafa9bb130df457e2285d1e0a3718d35a1664d1
ScreechingBagel <bagel@vlyn.org>
2025-11-15 01:01:44 +0100

    vomit some functions

---
+++ handle_email/handle_email.py
@@ -0,0 +1,27 @@
+def get_mail():
+    pass
+
+
+def get_new_mail():
+    pass
+
+
+def extract_link():
+    pass
+
+
+def extract_code():
+    pass
+
+
+def submit_confirmation():
+    # like different depending on if it's link or code?
+    pass
+
+
+def process_new_mail():
+    pass
+
+
+def log_result():
+    pass
---
+++ handle_users/handle_users.py
@@ -0,0 +1,14 @@
+def generate_user():
+    pass
+
+
+def generate_username():
+    pass
+
+
+def generate_password():
+    pass
+
+
+def save_user():
+    pass
