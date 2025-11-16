3dd7bc32e46c9b0b62a69eb6f73a079312c8176f
ScreechingBagel <bagel@vlyn.org>
2025-11-16 01:50:49 +0100

    Example: print subject of all mail in inbox
    using IMAPClient, had to also bump python version as workaround for https://github.com/astral-sh/uv/issues/15327

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import getpass
+
+from imapclient import IMAPClient
+
+HOST = "imap.mailbox.org"
+USERNAME = input("Username: ")
+PASSWORD = getpass.getpass()
+
+# basic ass MVP:
+# log into mailserver with IMAP,
+# fetch new mail,
+# extract all links,
+# print to stdout
+# using IMAPClient now
+
+
 def main():
-    print("Hello from mail!")
+    server = IMAPClient(HOST, use_uid=True)
+
+    server.login(USERNAME, PASSWORD)
+
+    select_info = server.select_folder("INBOX")
+    print("%d messages in INBOX" % select_info[b"EXISTS"])
+
+    messages = server.search("ALL")
+
+    print("%d messages from our best friend" % len(messages))
+
+    for msgid, data in server.fetch(messages, ["ENVELOPE"]).items():
+        envelope = data[b"ENVELOPE"]
+
+        print(envelope.subject.decode(), "\n")
+
+    server.logout()
 
 
 if __name__ == "__main__":
