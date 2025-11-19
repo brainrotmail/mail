e02ec6b62df78a5cff95a6b2333a2c6757048f80
ScreechingBagel <bagel@vlyn.org>
2025-11-19 01:58:24 +0100

    break into functions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import getpass
+from typing import Iterator
 
 from bs4 import BeautifulSoup
 from imap_tools import AND, MailBox
+from imap_tools.message import MailMessage
 
 HOST = "imap.gmx.org"
 USERNAME = input("Username: ")
@@ -12,16 +14,32 @@ FOLDER = "INBOX"
 def main():
     mailbox = MailBox(HOST)
     mailbox.login(USERNAME, PASSWORD, FOLDER)
-    for msg in mailbox.fetch(AND(all=True)):
+    inbox_dump = mailbox.fetch(AND(all=True))
+
+    process_mail(inbox_dump)
+
+
+def process_mail(email_bundle: Iterator[MailMessage]):
+    for msg in email_bundle:
         body = msg.html or msg.text or ""
 
-        urls = set()
-        soup = BeautifulSoup(body, "html.parser")
-        for link in soup.find_all("a", href=True):
-            urls.add(link["href"])
-
-        print(urls)
+        print(extract_urls(body))
         print("*** === ***")
+
+
+def extract_urls(body: str) -> set:
+    urls = set()
+    soup = BeautifulSoup(body, "html.parser")
+    for link in soup.find_all("a", href=True):
+        urls.add(link["href"])
+    return urls
+
+
+# so i need it to:
+# - detect new email (in loop? IDLE?)
+# - decide if email plaintext or hmtl
+# - extract URLs
+# - send http requests to all/some
 
 
 if __name__ == "__main__":
