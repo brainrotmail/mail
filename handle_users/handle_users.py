e03f15dbf3f1e7e3bb8575ba188ea9e2f79c05dd
ScreechingBagel <bagel@vlyn.org>
2025-11-19 16:27:56 +0100

    generate user data

--- handle_users/handle_users.py
+++ handle_users/handle_users.py
@@ -1,14 +1,33 @@
-def generate_user():
-    pass
+import secrets
+import string
 
 
-def generate_username():
-    pass
+def generate_user() -> tuple:
+    return (generate_username(), generate_password())
 
 
-def generate_password():
-    pass
+def generate_username() -> str:
+    alphabet = string.ascii_letters
+    username = "".join(secrets.choice(alphabet) for i in range(8))
+    return username
+
+
+def generate_password() -> str:
+    alphabet = string.ascii_letters + string.digits
+    while True:
+        password = "".join(secrets.choice(alphabet) for i in range(10))
+        if (
+            any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password) >= 3
+        ):
+            break
+    return password
 
 
 def save_user():
-    pass
+    password_store = open("passwords.sekrit", "a")
+    password_store.write(":".join(generate_user()))
+
+
+save_user()
